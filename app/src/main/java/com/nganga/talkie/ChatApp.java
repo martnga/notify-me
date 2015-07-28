c7658081ee5f5a0c0029ea85c32fef1f08715c88
mtnjoroge <mtnjoroge@gmail.com>
2015-07-28 17:44:35 +0300

    added the app ID and client KEY

--- app/src/main/java/com/nganga/talkie/ChatApp.java
+++ app/src/main/java/com/nganga/talkie/ChatApp.java
@@ -2,8 +2,16 @@ package com.nganga.talkie;
 
 import android.app.Application;
 
+import com.parse.Parse;
+
 /**
  * Created by nganga on 7/28/15.
  */
 public class ChatApp  extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "6nHlBQV1wqF6m4jH38t1ZmRI910llj9z18rgCg9A", "7bwd6qeStdZBsYoqGPTGQLGzDQbB4jW0DLPc0rDS");
+    }
 }
