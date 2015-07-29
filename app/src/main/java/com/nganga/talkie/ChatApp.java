9fb5bef1da401c4f638cf7a91d63b91230417936
mtnjoroge <mtnjoroge@gmail.com>
2015-07-29 21:43:38 +0300

    added the required permisions

--- app/src/main/java/com/nganga/talkie/ChatApp.java
+++ app/src/main/java/com/nganga/talkie/ChatApp.java
@@ -3,6 +3,7 @@ package com.nganga.talkie;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.PushService;
 
 /**
  * Created by nganga on 7/28/15.
@@ -13,5 +14,6 @@ public class ChatApp  extends Application{
     public void onCreate() {
         super.onCreate();
         Parse.initialize(this, "6nHlBQV1wqF6m4jH38t1ZmRI910llj9z18rgCg9A", "7bwd6qeStdZBsYoqGPTGQLGzDQbB4jW0DLPc0rDS");
+        PushService.setDefaultPushCallback(this,MainActivity.class);
     }
 }
